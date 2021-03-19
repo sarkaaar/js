a8836800bd7657748edadf8e2691ed0ba47e7f3c
Abdul Wali <waliabdul144@gmail.com>
2021-03-19 09:31:13 +0500

    JS and Styling

--- myjscode.js
+++ myjscode.js
@@ -1,24 +1,65 @@
-function submit_() {
-
+function checkName() {
   var name_validation = document.getElementById("name").value;
   name_len = name_validation.length;
 
-  if(name_len <= 4)
-    alert("Please Enter A Valid Name");
+  if(name_len <= 4) {
+    document.getElementById('name-p').innerText = "Value should be greater than 5";
+    document.getElementById('name').style.borderColor = "#DC2626";
+    return false;
+  } else {
+    document.getElementById('name-p').innerText = "";
+    document.getElementById('name').style.borderColor = "#9CA3AF";
+    return true;
+  }
+}
 
-  var pass_validation = document.getElementById("password").value;
-  pass_len = pass_validation.length;
+function checkPassword() {
+  let password = document.getElementById('password');
+  if(password.value.length < 8) {
+    password.style.borderColor = "#DC2626";
+    document.getElementById('password-p').innerText = "Password must be 8 characters long";
+    return false;
+  } else {
+    document.getElementById('password-p').innerText = "";
+    password.style.borderColor = "#9CA3AF";
+    return true;
+  }
+}
 
-  if(pass_len <= 8){
-    alert("Please Enter A Valid Password");
-    document.getElementById("password").style.border="2px solid red";
+function checkConfirmPassword() {
+  let confirm_password = document.getElementById('confirm-password');
+  let password = document.getElementById('password');
+  if(confirm_password.value !== password.value) {
+    document.getElementById('confirm-password-p').innerText = "Password doesn't match!";
+    confirm_password.style.borderColor = "#DC2626";
+    return false;
+  } else {
+    document.getElementById('confirm-password-p').innerText = "";
+    confirm_password.style.borderColor = "#9CA3AF";
+    return true;
+  }
+}
+
+window.addEventListener('load', ()=> {
+  document.getElementById('submit-form').addEventListener('click', submit);
+})
+
+function submit() {
+  checkName();
+  checkPassword();
+  checkConfirmPassword();
+  
+  let email = document.getElementById('email');
+
+  if(email.value.length == 0) {
+    document.getElementById('email-p').innerText = "Email is required";
+    email.style.borderColor = "#DC2626";
+  } else {
+    document.getElementById('email-p').innerText = "";
+    email.style.borderColor = "#9CA3AF";
   }
 
-  var conf_pass_validation = document.getElementById("confirm_password").value;
-
-  if (pass_validation != conf_pass_validation)
-    document.getElementById("confirm_password").style.border="2px solid red";
-    document.getElementById("password").style.border="2px solid red";
-    alert("Passwords do Not Match");
-
+  if(checkName() && checkPassword() && checkConfirmPassword()) {
+    console.log("Return");
+  } else return;
 }
